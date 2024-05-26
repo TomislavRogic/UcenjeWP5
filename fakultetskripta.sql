d735c48cf14368a57765214e06f830b2f643dd85
TomislavRogic <tomislav.rogic@gmail.com>
2024-05-26 14:17:02 +0200

    era dijagram fakultetskripta

--- fakultetskripta.sql
+++ fakultetskripta.sql
@@ -1,1 +1,23 @@
--- create database fakultet;
+﻿-- create database fakultet;
+
+-- use fakultet;
+
+-- izrada tablice Ispitni rok
+
+create table Ispitnirok(
+sifra int primary key not null,
+predmet varchar(50) not null,
+vrstaispita varchar(50) not null,
+datum date,
+pristupio varchar(50) not null
+);
+
+-- izrada tablice Pristupnici
+
+create table Pristupnici (
+Ispitnirok int not null,
+Student varchar(50) not null,
+Brojbodova varchar(3) not null,
+Ocjena char(1) not null
+);
+
