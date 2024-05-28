4fc961f8ddf071158516ee243a239c7fd885d204
TomislavRogic <tomislav.rogic@gmail.com>
2024-05-28 12:11:17 +0200

    zavrsni_rad_skripta

---
+++ Zavrsni_rad/zavrsni_rad.sql
@@ -0,0 +1,41 @@
+-- create database zavrsni_rad;
+
+--use zavrsni_rad;
+
+-- izrada tablice Osobe
+
+--create table Osobe(
+--sifraosoba int,
+--Ime varchar(30),
+--Prezime varchar(30),
+--Email varchar(50)
+--);
+
+-- izrada tablice Vozila
+
+--create table Vozila(
+--sifravozila int,
+--Marka varchar(100),
+--Vrstagoriva varchar(10),
+--Oblikkaroserije varchar(20),
+--Prvaregistracija int,
+--Godinaproizvodnje int,
+--Datumprveregistracije date,
+--Prijenos varchar(8),
+--Potrosnja varchar(15),
+--Boja varchar(20),
+--Opisvozila varchar(100),
+--Vlasnik varchar(10),
+--Cijena decimal(10,2)
+--);
+
+-- izrada tablice Termini
+
+--create table Termini(
+--Osobe int,
+--Vozila int
+--);
+
+
+
+
