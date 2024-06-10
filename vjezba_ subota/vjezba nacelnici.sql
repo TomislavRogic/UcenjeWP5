652b01474abc753eae9450f6c811f9ac7099f8e8
TomislavRogic <tomislav.rogic@gmail.com>
2024-06-10 12:03:19 +0200

    vjezba updatedelete

---
+++ vjezba_ subota/vjezba nacelnici.sql
@@ -0,0 +1,9 @@
+﻿create database bazaopcine;
+use bazaopcine;
+
+create table Načelnici(
+sifra int primary key not null identity(1,1),
+Ime varchar(50) not null,
+Prezime varchar (50) not null,
+Datumrodenja date
+);
