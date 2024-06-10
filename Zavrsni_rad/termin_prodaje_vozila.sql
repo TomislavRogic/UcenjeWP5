94a17d27443850ac88af3be1d1b183cc4e66cea7
TomislavRogic <tomislav.rogic@gmail.com>
2024-06-11 00:22:34 +0200

    dodavanje podataka u zavrsni rad

---
+++ Zavrsni_rad/termin_prodaje_vozila.sql
@@ -0,0 +1,86 @@
+use master;
+go
+
+drop database  if exists zavrsni_rad;
+go
+
+create database zavrsni_rad;
+go
+
+use zavrsni_rad;
+go
+
+
+ --izrada tablice Osobe
+
+create table Osobe(
+Sifraosoba int primary key not null identity(1,1),
+Ime varchar(30) not null,
+Prezime varchar(30) not null,
+Email varchar(50) not null
+);
+go
+
+-- izrada tablice Vozila
+
+create table Vozila(
+Sifravozila int primary key not null identity(1,1),
+Marka varchar(100) not null,
+Opisvozila varchar(100),
+Cijena decimal(10,3)
+);
+go
+
+-- izrada tablice Termini
+
+create table Termini(
+Sifratermina int primary key not null identity(1,1),
+Osobe int not null,
+Vozila int not null,
+Termin smalldatetime
+);
+go
+
+
+ --dodavanje foreign keya
+
+alter table Termini add foreign key (Osobe) references Osobe(sifraosoba);
+alter table Termini  add foreign key (Vozila) references Vozila(sifravozila);
+go
+
+-- dodavanje podataka u tablicu Osobe
+
+select * from Osobe;
+insert into Osobe (Ime,Prezime,Email) values
+('Tomislav','Rogic','tomislav.rogic@gmail.com'),
+('Kristina','Andric','kristinaadric13@gmail.com'),
+('Hans', 'Grubner','hans.grubner@siol.at'),
+('Markus','Walter','markus13@gmail.com'),
+('Pierce','Brosnan','pierce.brosnan@gmail.com'),
+('Milan','Prodanovic','milan.prodanovic@gmail.com');
+go
+
+
+--dodavanje podataka u tablicu Vozila
+
+select * from Vozila;
+
+insert into Vozila (Marka,Opisvozila,Cijena) values
+('Opel Mokka','2.2, dizel, 2017. godina, crna, 113.000 km, 5 sjedala, R17, prvi vlasnik',20.200),
+('Bmw X4','3.0, dizel, 2021.godina, bijela, 32.000 km, 5 sjedala, R 19, prvi vlasnik', 30.800),
+('Audi A4','2.2, dizel, 2020. godina, plava, 100.000 km, 5 sjedala, R 17, prvi vlasnik', 25.000),
+('Mazda CX5','2.2, benzin, 2021. godina, bijela, 54.000 km, 5 sjedala, R 19, prvi vlasnik',30.500);
+go
+
+-- dodavanje podataka u tablicu Termini
+
+select * from Termini;
+
+insert into Termini (Osobe,Vozila,Termin) values
+(1,1,'2024-06-10 10:00'),
+(2,4,'2024-06-10 12:00'),
+(3,1,'2024-06-11 13:00'),
+(6,3,'2024-06-10 15:00'),
+(5,2,'2024-06-11 09:30'),
+(4,2,'2024-06-11 11:30');  
+go
