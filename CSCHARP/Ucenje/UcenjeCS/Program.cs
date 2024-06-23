50e261c06193e24fa3a6a7d5ca2b47ee704120a3
TomislavRogic <tomislav.rogic@gmail.com>
2024-06-23 15:03:49 +0200

    Literatura CSHARP

--- CSCHARP/Ucenje/UcenjeCS/Program.cs
+++ CSCHARP/Ucenje/UcenjeCS/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Edunova!");
+Console.WriteLine("Pozdrav Hrvatska!");
+
+// Ovo je komentar jedna linija
+
+/*
+ 
+Komentar kroz vise linija
+]]]]]]]
+
+
+
+*/
+
+Console.WriteLine("Zadnja linija čžđŽĐ");
