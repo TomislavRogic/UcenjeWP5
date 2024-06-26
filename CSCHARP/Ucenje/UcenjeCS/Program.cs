ef2734423b17357e2a514d73dec4ed3b155210de
TomislavRogic <tomislav.rogic@gmail.com>
2024-06-26 23:08:53 +0200

    UcenjeCS

--- CSCHARP/Ucenje/UcenjeCS/Program.cs
+++ CSCHARP/Ucenje/UcenjeCS/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Edunova!");
+Console.WriteLine("Zagreb");      
+
+// ovo je komentar jedna linija 
+/*
+
+
+Komentar kroz vise linija 
+
+
+*/
