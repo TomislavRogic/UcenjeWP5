a1a2f38afbd5d246c74c2d1dc9d389d937f5df04
TomislavRogic <tomislav.rogic@gmail.com>
2024-07-09 08:13:10 +0200

    E0901Vjezba_ispis_imena_slovo_jedno_ispod_drugog

---
+++ CSCHARP/Ucenje/UcenjeCS/E0901Vjezba.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class E0901Vjezba
+    {
+        // kako ispisati ime slova jedno ispod drugog
+        public static void Izvedi()
+        {
+            string ime = "Tomislav";
+            foreach(char slovo in ime)
+            {
+                Console.WriteLine(slovo);
+            }
+        }
+
+    }
+}
