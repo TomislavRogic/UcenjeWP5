e64c49e06a21773886ab7e15792e12ea195f77a5
TomislavRogic <tomislav.rogic@gmail.com>
2024-08-08 17:39:59 +0200

    zavrsen kod za ciklicku tablicu lijevo dole u smjeru kazaljke na satu

--- CSCHARP/Ucenje/UcenjeCS/E06Ciklickatablicadoljelijevousmjerukazaljke.cs
+++ CSCHARP/Ucenje/UcenjeCS/E06Ciklickatablicadoljelijevousmjerukazaljke.cs
@@ -10,105 +10,134 @@ namespace UcenjeCS
     {
         public static void Izvedi()
         {
-            int brojredova = 0;
-            int brojkolona = 0;
-
-            while (true)
+            string pitanje;
+            int brojkreiranihtablica=0;
+            do
             {
-                try
-                {
-                    Console.WriteLine("Molim unesite broj redova u tablici izmedu 2 i 50: ");
-                    brojredova = int.Parse(Console.ReadLine());
-                    if (brojredova >= 2 && brojredova <= 50)
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Uneseni broj redova nije izmedu 2 i 50, molim Vas unesite ponovo");
-                    }
-                }
-                catch
-                {
-                    Console.WriteLine("Neispravan unos. Molim unesite broj redova izmedu 2 i 50.");
-                }
+                int brojredova = 0;
+                int brojkolona = 0;
 
-            }
-            while (true)
-            {
-                try
+                while (true)
                 {
-                    Console.WriteLine("Molim unesite broj kolona u tablici izmedu 2 i 50:");
-                    brojkolona = int.Parse(Console.ReadLine());
-                    if(brojkolona>=2 && brojkolona <= 50)
+                    try
                     {
-                        break;
+                        Console.WriteLine("Molim unesite broj redova u tablici izmedu 2 i 50: ");
+                        brojredova = int.Parse(Console.ReadLine());
+                        if (brojredova >= 2 && brojredova <= 50)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Uneseni broj redova nije izmedu 2 i 50, molim Vas unesite ponovo");
+                        }
                     }
-                    else
+                    catch
                     {
-                        Console.WriteLine("Uneseni broj redova nije izmedu 2 i 50, molim Vas unesite ponovo");
+                        Console.WriteLine("Neispravan unos. Molim unesite broj redova izmedu 2 i 50.");
                     }
-                }
-                catch
-                {
-                    Console.WriteLine("Neispravan unos. Molim unesite broj kolona izmedu 2 i 50.");
-                }
-            }
-            int[,] ciklickatablica = new int[brojredova, brojkolona];
-            int broj = 2;
-            int gore = 0;
-            int dole = brojredova - 1;
-            int lijevo = 0;
-            int desno=brojkolona - 1;
-            while(gore<=dole && lijevo <= desno)
-            {// dole prema gore
-                for(int i = dole; i >= gore; i--)
-                {
-                    
-                    ciklickatablica[i, lijevo] = broj++;
-                }
-                lijevo++;
-                //lijevo prema desno
-                for(int j=lijevo; j<=desno; j++)
-                {
-                    
-                        ciklickatablica[gore,j] = broj++;
-                }
-                gore++;
 
-                // gore prema dole
-                for (int i=gore; i <= dole; i++)
+                }
+                while (true)
                 {
-                    
+                    try
+                    {
+                        Console.WriteLine("Molim unesite broj kolona u tablici izmedu 2 i 50:");
+                        brojkolona = int.Parse(Console.ReadLine());
+                        if (brojkolona >= 2 && brojkolona <= 50)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Uneseni broj redova nije izmedu 2 i 50, molim Vas unesite ponovo");
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Neispravan unos. Molim unesite broj kolona izmedu 2 i 50.");
+                    }
+                }
+                int[,] ciklickatablica = new int[brojredova, brojkolona];
+                int broj = 2;
+                int gore = 0;
+                int dole = brojredova - 1;
+                int lijevo = 0;
+                int desno = brojkolona - 1;
+                while (gore <= dole && lijevo <= desno)
+                {// dole prema gore
+                    for (int i = dole; i >= gore; i--)
+                    {
+
+                        ciklickatablica[i, lijevo] = broj++;
+                    }
+                    lijevo++;
+                    //lijevo prema desno
+                    for (int j = lijevo; j <= desno; j++)
+                    {
+
+                        ciklickatablica[gore, j] = broj++;
+                    }
+                    gore++;
+
+                    // gore prema dole
+                    for (int i = gore; i <= dole; i++)
+                    {
+
                         ciklickatablica[i, desno] = broj++;
-                }
-                desno--;
+                    }
+                    desno--;
 
-                //desno prema lijevo
-                for(int j=desno; j>=lijevo; j--)
-                {
-                    
+                    //desno prema lijevo
+                    for (int j = desno; j >= lijevo; j--)
+                    {
+
                         ciklickatablica[dole, j] = broj++;
-                }
-                dole--;
+                    }
+                    dole--;
 
-            }
-            for (int i = 0; i < brojredova; i++)
-            {
-                for (int j = 0; j < brojkolona; j++)
+                }
+                for (int i = 0; i < brojredova; i++)
                 {
-                    Console.Write(String.Format("{0,3}", ciklickatablica[i, j]) + " ");
+                    for (int j = 0; j < brojkolona; j++)
+                    {
+                        Console.Write(String.Format("{0,3}", ciklickatablica[i, j]) + " ");
+                    }
+                    Console.WriteLine();
                 }
-                Console.WriteLine();
-            }
+                brojkreiranihtablica = brojkreiranihtablica + 1;
 
+                // unos dodatnog pitanja da li korisnik zeli napraviti jos jednu tablicu
 
+                // Provjera broja kreiranih tablica
+                if (brojkreiranihtablica >= 2)
+                {
+                    Console.WriteLine("Napravili ste maksimalan broj cikličkih tablica. To bi bilo sve za danas :) !");
+                    break;
+                }
 
+                // Unos pitanja o kreiranju nove tablice
+                bool ispravanUnos = false;
+                while (!ispravanUnos)
+                {
+                    Console.WriteLine("Želite li napraviti još jednu tablicu? (da/ne) ");
+                    pitanje = Console.ReadLine().ToLower();
 
+                    switch (pitanje)
+                    {
+                        case "da":
+                            ispravanUnos = true;
+                            break;
+                        case "ne":
+                            Console.WriteLine("Hvala Vam na Vašem vremenu. Želimo Vam ugodan dan!");
+                            return; // Izlazak iz metode
+                        default:
+                            Console.WriteLine("Neispravan unos. Molim odgovorite s 'da' ili 'ne'.");
+                            break;
+                    }
+                }
 
-
-
-
+            } while (true);
         }
 
 
