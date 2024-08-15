702b12e9dfe938eec212e94a6980fcff2c92d4db
TomislavRogic <tomislav.rogic@gmail.com>
2024-08-15 11:49:03 +0200

    E1Instrukcije

---
+++ CSCHARP/Ucenje/UcenjeCS/E1Zadatakinstrukcije.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class E1Zadatakinstrukcije
+    {
+        public static void Izvedi()
+        {
+            string ime = "Marko";
+            char prvoslovo = ime[0];
+            string prezime = "Markic";
+
+            string imeprezime = ime + " " + prezime; //MarkoMarkic
+            imeprezime = imeprezime.ToLower();
+            //string tekst= Console.ReadLine();
+            //Console.WriteLine(tekst);
+
+            int[] ponavljanjeslova = new int[26]; // [0:A, 1: B,...]
+
+            for (int i =0; i < imeprezime.Length; i++)
+            {
+                char slovo = imeprezime[i]; // broji za slovo
+            }
+
+        }
+    }
+}
