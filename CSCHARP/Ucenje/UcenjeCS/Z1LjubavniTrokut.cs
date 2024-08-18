1db8866679c61b20ddea27cab7b755e9c103e01b
TomislavRogic <tomislav.rogic@gmail.com>
2024-08-18 16:52:59 +0200

    Ljubavnikalkulatorrjesavanjezadatka

--- CSCHARP/Ucenje/UcenjeCS/Z1LjubavniTrokut.cs
+++ CSCHARP/Ucenje/UcenjeCS/Z1LjubavniTrokut.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,31 +20,47 @@ namespace UcenjeCS
             string partnerovoime= Console.ReadLine();
 
             // spajanje ta dva imena u jedno
-            string imepar= vaseime + "" + partnerovoime;
+            string imepar= vaseime + "  " + partnerovoime;
             
-            // brojanje pojavljivanje slova-- provjerava u ascii i svaki put oduzima asci broj od 'a' i onda broju dodijeljuje vrijednost
-            int[] count= new int[26];
+
+            // brojanje pojavljivanje slova-- dva imena ubacujemo u dictionary i onda radimo petlju koja provjerava da li slovo vec postoji, ako postoji inetarcija se povecava za 1 a ako ne vrijednot je 1
+            Dictionary<char, int> letterCounts = new Dictionary<char, int>();
             foreach (char slovo in imepar.ToLower())
             {
                 if (char.IsLetter(slovo))
                 {
-                    count[slovo - 'a']++;
+                    if (letterCounts.ContainsKey(slovo))
+                    {
+                        letterCounts[slovo]++;
+                    }
+                    else
+                    {
+                        letterCounts[slovo] = 1;
+                    }
                 }
             }
 
-            // ispis slova i koliko puta se pojavljuje
-            foreach (char slovo in imepar)
+            // Print each character as it is
+            foreach (char slovo in imepar.ToLower())
             {
-                if (char.IsLetter(slovo))
-                {
-                    int index = slovo - 'a';
-                    Console.WriteLine($"{slovo}"); // ispisuje slovo
-                    Console.WriteLine($"{count[index]}");
-                }
-
+                Console.Write(slovo);
             }
+            Console.WriteLine(); // Move to the next line
 
-
+            // Print the count of each character in the same order
+            // Print the count of each character in the same order
+            foreach (char slovo in imepar.ToUpper())
+            {
+                if (char.IsLetter(slovo)) // Ignore non-letter characters
+                {
+                    Console.Write(letterCounts[char.ToLower(slovo)]);
+                }
+                else
+                {
+                    Console.Write(' '); // Maintain spaces in the output
+                }
+            }
+            Console.WriteLine(); // Move to the next line
 
 
 
