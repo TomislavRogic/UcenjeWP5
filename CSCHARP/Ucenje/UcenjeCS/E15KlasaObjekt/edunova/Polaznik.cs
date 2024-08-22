3fd12093bf021d762f7461ce302025c27bb6c08d
TomislavRogic <tomislav.rogic@gmail.com>
2024-08-22 15:49:43 +0200

    OOP klasa objekt, ucahurivanje

---
+++ CSCHARP/Ucenje/UcenjeCS/E15KlasaObjekt/edunova/Polaznik.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS.E15KlasaObjekt.edunova
+{
+    internal class Polaznik
+    {
+        public int? Sifra { get; set; }
+        public string? Ime { get; set; }
+        public string? Prezime { get; set; }
+        public string? Email { get; set; }
+        public string? Smjer { get; set; }
+        public string? Predavac { get; set; }
+        public Polaznik[]? Polaznici { get; set; }
+
+    }
+}
