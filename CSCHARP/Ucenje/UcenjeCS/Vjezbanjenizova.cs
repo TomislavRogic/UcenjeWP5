cf8abd91aaa7a8f823f899589f599fabb7ec99f2
TomislavRogic <tomislav.rogic@gmail.com>
2024-09-01 18:04:53 +0200

    Vjezbanje primjera s nizovima

--- CSCHARP/Ucenje/UcenjeCS/Vjezbanjenizova.cs
+++ CSCHARP/Ucenje/UcenjeCS/Vjezbanjenizova.cs
@@ -12,27 +12,32 @@ namespace UcenjeCS
         {
             // Zadatak 1. Ispisati brojeve iz niza
 
-           /* int[] PrviNiz = { 1, 2, 3, 4, 5 };
+            /* int[] PrviNiz = { 1, 2, 3, 4, 5 };
 
-            for (int i = 0; i < PrviNiz.Length; i++)
-            {
-                Console.WriteLine(i + " Broj u nizu je: " + PrviNiz[i]);
-                
-            }
-           */
+             for (int i = 0; i < PrviNiz.Length; i++)
+             {
+                 Console.WriteLine(i + " Broj u nizu je: " + PrviNiz[i]);
+
+             }
+            */
 
             // Zadatak 2. Unijeti 10 brojeva u niz i ispisati svaki s foreach petljom
-            int[] DrugiNiz = new int[10] ;
-            Console.WriteLine("Molim unesite brojeve: ");
-            int brojevi = int.Parse(Console.ReadLine());
 
+            int[] DrugiNiz = new int[10];
+            Console.WriteLine("Molim unesite 10 brojeva: ");
+
+
+            
+            for (int i=0; i<DrugiNiz.Length; i++)
+            {
+                DrugiNiz[i]= int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Brojevi koje ste unijeli su: ");
             foreach (int broj in DrugiNiz)
             {
                 Console.WriteLine(broj);
             }
-
-
-
 
 
 
