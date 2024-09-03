dce05feee341bf3f8c477dbb5e6349c82afa4946
TomislavRogic <tomislav.rogic@gmail.com>
2024-09-03 13:14:07 +0200

    Zavrsni rad Zadatak

--- CSCHARP/Ucenje/UcenjeCS/E16NasljedivanjePolimorfizam/ZavrsniRad/Zadatak.cs
+++ CSCHARP/Ucenje/UcenjeCS/E16NasljedivanjePolimorfizam/ZavrsniRad/Zadatak.cs
@@ -8,19 +8,39 @@ namespace UcenjeCS.E16NasljedivanjePolimorfizam.ZavrsniRad
 {
     internal class Zadatak
     {
+        public Osoba? osoba;
+        public Vozilo? vozilo;
+        public Termin? Termin;
 
         /*
          * 
-         * Kreirati klase prema ERA dijagramu Vaše baze podataka
-         * Implementirati nasljeđivanje i prepisati metodu ToString
+         * Kreirati klase prema ERA dijagramu Vaše baze podataka - napravio
+         * Implementirati nasljeđivanje i prepisati metodu ToString- napravio
          * 
-         * Za svaku klasu napraviti po jedan konstruktor
+         * Za svaku klasu napraviti po jedan konstruktor- napravio  
          * 
          * Svaku klasu instancirati u ovoj klasu
-         * 
          */
-        public Zadatak() {
+
+
+
+        public Zadatak() 
+        {
             // //ovdje instancirati svaku kreiranu klasu jednom i dodjeliti svim svojstvima vrijednosti
+
+            Osoba osoba = new Osoba(1, "Marko", "Peric", "markoperic@gmail.com");
+            Vozilo vozilo = new Vozilo(1, "Mazda", "CX5", 16000);
+            Termin Termin = new Termin(1, osoba, vozilo, DateTime.Now);
+
+
+            // testiranje
+            Console.WriteLine(osoba.ToString());
+            Console.WriteLine(vozilo.ToString());
+            Console.WriteLine(Termin.ToString());
+
+
+
+
         }
     }
 }
