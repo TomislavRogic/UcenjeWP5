9791e89a697d956ad08cfd1f91a527a89b5ce78b
TomislavRogic <tomislav.rogic@gmail.com>
2024-09-03 20:02:29 +0200

    Generici

---
+++ CSCHARP/Ucenje/UcenjeCS/E17GenericiLambdaEkstenzije/Obrada.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS.E17GenericiLambdaEkstenzije
+{
+    internal class Obrada<T> where T:Entitet
+    {
+        // uobicajeno je krenuti s slovom T
+        public T? ObjektObrade { get; set; }
+
+        public void Obradi()
+        {
+            Console.WriteLine(ObjektObrade?.Sifra);
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ CSCHARP/Ucenje/UcenjeCS/E17GenericiLambdaEkstenzije/Smjer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS.E17GenericiLambdaEkstenzije
+{
+    internal class Smjer:Entitet
+    {
+       
+        public string? Naziv { get; set; }
+
+
+
+
+
+
+
+    }
+}
