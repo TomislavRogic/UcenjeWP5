c6ed2be910ec3946468fd16666ed8c9d3e45ee09
TomislavRogic <tomislav.rogic@gmail.com>
2024-09-27 13:50:22 +0200

    Instrukcije

--- CSCHARP/Ucenje/UcenjeCS/E03FunkcijaKojaVracaVrijednost.cs
+++ CSCHARP/Ucenje/UcenjeCS/E03FunkcijaKojaVracaVrijednost.cs
@@ -16,22 +16,34 @@ namespace UcenjeCS
             //Console.WriteLine(duploVeciBroj);
 
             //Console.WriteLine(VratiDuploVeciBroj(6));
-            Console.WriteLine("Molim unesi dva broja");
+            //Console.WriteLine("Molim unesi dva broja");
 
-            int prvibroj = int.Parse(Console.ReadLine());
+            /*int prvibroj = int.Parse(Console.ReadLine());
             int drugibroj= int.Parse(Console.ReadLine());
-            Console.WriteLine(dvaBroja(prvibroj,drugibroj));
+            Console.WriteLine(dvaBroja(prvibroj,drugibroj));*/
+
+            //zadatak
+            int[] petbrojeva = new int[5];
+            Console.WriteLine("Molim Vas unesite 5 brojeva");
+            for (int i = 0; i < 5; i++)
+            {
+                petbrojeva[i] = int.Parse(Console.ReadLine());
+            }
+
+            // Poziv metode koja vraća brojeve veće od 10
+            PetBrojeva(petbrojeva);
+
 
         }
 
-        public static int VratiDuploVeciBroj(int broj)
+        /*public static int VratiDuploVeciBroj(int broj)
         {
             return broj * 2;
-        }
+        }*/
 
         // fja prima dva broja i vraca veci broj. Ispisati taj broj u Izvedi
 
-        public static int dvaBroja(int a, int b)
+        /*public static int dvaBroja(int a, int b)
         {
             if (a > b)
             {
@@ -41,9 +53,24 @@ namespace UcenjeCS
             {
                 return b;
             }
-        }
+        }*/
 
         // korisnik upisuje 5 brojeva, spremi ih u polje i proslijedi funkciji koja ce ispisati sve brojeve vece od 10.
-            
+
+        public static void PetBrojeva(int[] brojevi)
+        {
+            foreach (int broj in brojevi)
+            {
+                if (broj > 10)
+                {
+                    Console.WriteLine(broj);
+                }
+            }
+          
+        }
+
+        
+
+
     }
 }
---
+++ CSCHARP/Ucenje/UcenjeCS/VjezbanjeRekurzivnaFunkcija.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class VjezbanjeRekurzivnaFunkcija
+    {
+
+        public static void Izvedi()
+        {
+            // rekurzivna funkcija poziva samu sebe
+            // moramo imati uvijet zaustavljanja rekurzivne funkcije
+            // rekurzivni poziv šalje izmijenjene parametre
+            //IspisiBok(3);
+
+
+            Console.WriteLine(IzracunajFaktorijelu(3));
+        }
+
+        public static void IspisiBok(int broj)
+        {
+            Console.WriteLine("Bok");
+            broj -= 1;
+
+            if (broj > 0)
+            {
+                IspisiBok(broj);
+            }
+            // funkcija koja ne vraca nista ne mora imati return
+        }
+
+        // 3! = 1 * 2 * 3 
+        // 5! = 1 *2 *3 * 4 * 5
+        public static int IzracunajFaktorijelu(int n)
+        {
+            // F(3) = 3 * F(2)
+            // F(2) =  2 * F(1)
+            // F(1) = 1
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return n * IzracunajFaktorijelu(n - 1);
+        }
+
+        // Fib(n) = Fib
+        // F(1) = 1, F(2) = 1
+        // F(n) = F(n-1) + F(n-2)
+
+        // F(4) = F(3) + F(2)
+        // F(4) = F(3) + 1 => F(3) = 1 * 1
+        // Fin = 1, 1, 2, 3, 5, 8, 13
+    }
+}
